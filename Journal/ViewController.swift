35479c2e08183a11722ceaea90899f57add05559
dnstrach <dnstrach@gmail.com>
2023-02-28 21:19:38 -0800

    Initial Commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Dominique Strachan on 2/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
